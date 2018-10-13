665f8d3a0bded2fba64dcf80de5b11d09896a7c8
Clol7 <dav.trounson@gmail.com>
2018-10-13 16:43:48 +1100

    New ResultCompare class, these will hold the title, content and results of the web search/database comparison to original document. Not currently hooked up to anything

---
+++ PlagiarismDetection/src/main/java/PlagiarismDetection/ResultCompare.java
@@ -0,0 +1,49 @@
+/*
+ * CSIT214/814 GROUP ALPHA
+ */
+
+package PlagiarismDetection;
+
+
+public class ResultCompare {
+    String title;
+    String content;
+    double percent;
+    
+    ResultCompare(String title, String content){
+        this.content = content;
+        this.title = title;
+        
+        double titlePercent = calcPercent(title);
+        double contentPercent = calcPercent(content);
+        
+        this.percent = (titlePercent + contentPercent) / 2;
+    }
+    
+    static double calcPercent(String content){
+        double percentContent = 0;
+        String comparativeContent = "";
+
+        if(content.equals(comparativeContent)){
+            return 100;
+        }
+        
+        if(content.length() == 0 || comparativeContent.length() == 0){
+            return 0;
+        }
+        
+        double maxLen = content.length() > comparativeContent.length() ? content.length() : comparativeContent.length();
+        int minLen = content.length() < comparativeContent.length() ? content.length() : comparativeContent.length();
+        int sameCharLength = 0;
+        
+        for(int i = 0; i < minLen; i++){
+            if(content.charAt(i) == comparativeContent.charAt(i)){
+                sameCharLength++;
+            }
+        }
+        
+        percentContent = sameCharLength / maxLen * 100;
+        
+        return percentContent;
+    }
+}
